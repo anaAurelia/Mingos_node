79cc1e5b767686f94b791e976bc79a971ee80bf6
Ana Aurelia Muñiz <aurelana75@gmail.com>
2022-07-21 08:10:52 -0500

    PARTE 1 - COMPLETADA

---
+++ routes/router.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+
+//Llamando a nuestra conexion de base de datos 
+const conexion = require('../database/db')
+
+router.get('/', (req, res)=>{
+    conexion()
+    res.render('index')
+})
+
+router.get('/', (req, res)=>{
+    res.render('index')
+})
+
+router.get('/login', (req, res)=>{
+    res.render('login')
+})
+
+router.get('/register', (req, res)=>{
+    res.render('register')
+})
+
+module.exports =  router
