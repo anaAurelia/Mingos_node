8e1adc87f00a97399db8a0e24a014769757c2e48
Ana Aurelia Muñiz <aurelana75@gmail.com>
2022-07-21 14:29:50 -0500

    Creación de la bd y la creación de las vistas

---
+++ database/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql')
+
+const conexion = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '',
+    database : 'nodee',
+})
+
+conexion.connect( (error)=>{
+    if(error){
+        console.log('El error de conexion es: '+error)
+        return 
+    }
+    console.log('¡Conectado a la base de datos!')
+})
+
+module.exports = conexion
--- routes/router.js
+++ routes/router.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
     res.render('index')
 })
 
-router.get('/login', (req, res)=>{
+router.get('/login', (req, res) => {
     res.render('login')
 })
 
-router.get('/register', (req, res)=>{
+router.get('/register', (req, res) => {
     res.render('register')
 })
 
