96a6ac6170bb38a980b4ac9c91f2addb9738ccc2
Ana Aurelia Muñiz <aurelana75@gmail.com>
2022-07-21 16:54:17 -0500

    Ya registra los usuarios

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,24 @@
 const jwt = require('jsonwebtoken')
 const bcryptjs = require('bcryptjs')
 const conexion = require('../database/db')
-const{promisify} = require('util')
+const { promisify } = require('util')
 
 //procedimiento para registrarnos
-exports.register = async(req, res) =>{
-const name = req.body.name
-const user = req.body.user
-const pass = req.body.pass
+exports.register = async (req, res) => {
+    try {
+        const name = req.body.name
+        const user = req.body.user
+        const pass = req.body.pass
+        let passHash = await bcryptjs.hash(pass, 8)
+        //console.log(passHash)
+        conexion.query('INSERT INTO users SET ?', {user:user, name:name, pass:passHash},(error, results)=>{
+          if(error){console.log(error)}
+          res.redirect('/')
+        })
+    } catch (error) {
+        console.log(error)
+    }
 
-console.log(name + " - " + user + " - " + pass)
+
+
 }
